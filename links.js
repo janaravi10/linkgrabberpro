09afd7e0e96886ffe067dba33940bc8af97ac6b7
janaravi10 <janaravi962@gmail.com>
2018-07-06 10:44:23 +0530

    remove search function which is added by util script

--- links.js
+++ links.js
@@ -160,31 +160,4 @@
 
         })
     }
-    let searchBox = document.querySelector("input#filter");
-    searchBox.addEventListener("keyup", handleSearch);
-    function handleSearch(e) {
-        let links = document.querySelectorAll("a.link"), value = searchBox.value.trim().toLowerCase();
-        valueForReg = value;
-        valueForReg = valueForReg.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
-        if (value == "") { if (e.keyCode !== 8) return; };
-        let i, len = links.length;
-        for (i = 0; i < len; i++) {
-            link = links[i];
-            linkCon = link.parentElement, clsList = linkCon.classList;
-            clsList.remove("hideLinkCon", "showThis");
-            if (RegExp(valueForReg, 'i').test(link.innerText) === false) {
-                clsList.add("hideLinkCon");
-            } else {
-                clsList.add("showThis");
-                valueForLink = link.innerText.replace(RegExp(valueForReg, 'gi'), `<span class='lightup'>${value}</span>`);
-                link.innerText = "";
-                link.insertAdjacentHTML("beforeend", valueForLink);
-            }
-        }
-        let putNum = document.getElementsByClassName("div.showThis");
-        let n, plen = putNum.length;
-        for (n = 0; n < plen; n++) {
-            putNum[n].firstElementChild.innerText = n + 1;
-        }
-    }
 })()
